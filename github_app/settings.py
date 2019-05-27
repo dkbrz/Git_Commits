47ab306c5898dd1078250130a6a2c4aad23e4e32
dkbrz <glaz.dikobraz@gmail.com>
2019-05-27 11:14:36 +0300

    settings

---
+++ github_app/settings.py
@@ -0,0 +1,13 @@
+CONFIG = {
+    'USER': 'root',
+    'PASSWORD': 'password',
+    'HOST': 'localhost',
+    'PORT': 3306,
+    'DATABASE': 'pruned_commits_db2'
+}
+
+APP_ROOT = '/home/dkbrz/GitHub/Git_Commits/github_app'
+
+LINK_PREFIX = ''
+
+W2V = '/home/dkbrz/GitHub/Git_Commits/github_app/data/vectors.txt'
